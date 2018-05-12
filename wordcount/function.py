9feb4d18ef43400aab1acc8cfd4355955b5bd427
hushunxin <llyzhsx@163.com>
2018-05-12 16:04:55 +0800

    制作完成字数统计网站

---
+++ wordcount/function.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def count(request):
+    total_count = len(request.GET['text'])
+    user_text = request.GET['text']
+
+    word_dict = {}
+
+    for word in user_text:
+        if word not in word_dict:
+            word_dict[word] = 1
+        else:
+            word_dict[word] += 1
+
+    sorted_dict = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
+
+    return render(request, "count.html", {'count': total_count,
+                                          'text': user_text,
+                                          'wordict': word_dict,
+                                          'sorted': sorted_dict})
+
+
+def about(request):
+    return render(request, "about.html")
